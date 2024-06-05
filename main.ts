54ac70fb97ae8ea1cff98f11d2d091910aa9bf85
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2024-06-05 08:43:37 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 || maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorStop(maqueen.Motors.All)
+        music.play(music.stringPlayable("C5 B A G F E D C ", 300), music.PlaybackMode.UntilDone)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    }
+})
